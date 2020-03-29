89452472a385e44325fc3bd237aec830a13d945b
Tinram <Tinram@users.noreply.github.com>
2020-03-29 18:20:55 +0100

    update mysql_vars.sh

--- mysql_vars.sh
+++ mysql_vars.sh
@@ -7,26 +7,26 @@
 
 # author      Martin Latter
 # copyright   Martin Latter 24/11/2016
-# version     0.14
+# version     0.15
 # license     GNU GPL version 3.0 (GPL v3); http://www.gnu.org/licenses/gpl.html
 # link        https://github.com/Tinram/MySQL.git
 
 
-version="v.0.14"
-date="20170620"
+version="v.0.15"
+date="20200329"
 mysqlPort="3306" # for remote host not listening on 3306; for 'localhost' MySQL connects with sockets (override: 127.0.0.1)
 scriptName=$0
 
 
-showHelp() {
-
+showHelp()
+{
 	echo -e "\nmysql_vars $version $date\ndisplay important mysqld variables\n"
 	echo -e "usage: $scriptName\n"
 }
 
 
-main() {
-
+main()
+{
 	if [[ $1 == "-h" || $1 == "--help" ]]; then
 		showHelp
 		exit 1
@@ -56,11 +56,15 @@ main() {
 		SHOW VARIABLES LIKE 'character_set_%';
 		SHOW VARIABLES LIKE 'collation%'";
 
-	echo -e "\n\n~~ CONNECTIONS ~~\n"
+	echo -e "\n\n~~ CONNECTIONS AND THREADS ~~\n"
 	mysql -u$mysqlUser -p$mysqlPass -h$mysqlHost -P$mysqlPort -e "
 		SHOW STATUS LIKE '%connect%';
+		SHOW VARIABLES WHERE variable_name = 'max_allowed_packet';
+		SHOW VARIABLES WHERE variable_name = 'thread_cache_size';
+		SHOW STATUS WHERE variable_name = 'threads_connected';
+		SHOW STATUS WHERE variable_name = 'threads_running';
 		SHOW VARIABLES WHERE variable_name = 'max_connections';
-		SHOW VARIABLES WHERE variable_name = 'max_allowed_packet'";
+		SHOW VARIABLES WHERE variable_name = 'wait_timeout'";
 
 	echo -e "\n\n~~ TIMEOUTS ~~\n"
 	mysql -u$mysqlUser -p$mysqlPass -h$mysqlHost -P$mysqlPort -e "
@@ -71,20 +75,14 @@ main() {
 		SHOW STATUS LIKE 'Open%tables';
 		SHOW VARIABLES WHERE variable_name = 'table_open_cache'";
 
-	echo -e "\n\n~~ THREADS ~~\n"
-	mysql -u$mysqlUser -p$mysqlPass -h$mysqlHost -P$mysqlPort -e "
-		SHOW VARIABLES WHERE variable_name = 'thread_cache_size';
-		SHOW STATUS WHERE variable_name = 'threads_connected';
-		SHOW STATUS WHERE variable_name = 'threads_running'";
-
 	echo -e "\n\n~~ MYISAM KEY VARS ~~\n"
 	mysql -u$mysqlUser -p$mysqlPass -h$mysqlHost -P$mysqlPort -e "
 		SHOW VARIABLES WHERE variable_name = 'myisam_sort_buffer_size';
-		SHOW VARIABLES WHERE variable_name = 'key_buffer_size';
-		SELECT
-			(SELECT variable_value FROM information_schema.global_status WHERE variable_name = 'key_reads') AS key_reads,
-			(SELECT variable_value FROM information_schema.global_status WHERE variable_name = 'key_read_requests') AS kr_requests,
-			(SELECT (1 - key_reads / kr_requests) * 100) AS key_cache_hit_percentage";
+		SHOW VARIABLES WHERE variable_name = 'key_buffer_size'";
+		#SELECT
+			#(SELECT variable_value FROM information_schema.global_status WHERE variable_name = 'key_reads') AS key_reads,
+			#(SELECT variable_value FROM information_schema.global_status WHERE variable_name = 'key_read_requests') AS kr_requests,
+			#(SELECT (1 - key_reads / kr_requests) * 100) AS key_cache_hit_percentage";
 
 	echo -e "\n\n~~ INNODB KEY VARS ~~\n"
 	mysql -u$mysqlUser -p$mysqlPass -h$mysqlHost -P$mysqlPort -e "
@@ -111,9 +109,9 @@ main() {
 		SHOW STATUS WHERE variable_name LIKE 'innodb_log_%';
 		SHOW STATUS WHERE variable_name LIKE 'innodb_row%'";
 
-		#echo -e "\n\n~~ INNODB STATUS ~~\n"
-		#mysql -u$mysqlUser -p$mysqlPass -h$mysqlHost -P$mysqlPort -e "
-		#SHOW ENGINE INNODB STATUS;"
+	#echo -e "\n\n~~ INNODB STATUS ~~\n"
+	#mysql -u$mysqlUser -p$mysqlPass -h$mysqlHost -P$mysqlPort -e "
+	#SHOW ENGINE INNODB STATUS;";
 
 	echo -e "\n\n~~ KEYS ~~\n"
 	mysql -u$mysqlUser -p$mysqlPass -h$mysqlHost -P$mysqlPort -e "
