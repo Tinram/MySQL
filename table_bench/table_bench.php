84c8acc3c5830651e04868e8c67aa07f18489e65
Tinram <Tinram@users.noreply.github.com>
2020-12-02 16:01:58 +0000

    update table_bench: table_bench.php

--- table_bench/table_bench.php
+++ table_bench/table_bench.php
@@ -2,18 +2,19 @@
 <?php
 
 /**
-	* Execute TableBench class using values from configuration file.
+    * Execute TableBench class using values from configuration file.
 */
+
+declare(strict_types=1);
 
 require('config.php');
 require('tablebench.class.php');
 
 
-if (PHP_SAPI !== 'cli') {
-	header('Content-Type: text/html; charset=utf-8');
+if (PHP_SAPI !== 'cli')
+{
+    header('Content-Type: text/html; charset=utf-8');
 }
 
 $oTF = new TableBench();
 echo $oTF->getResults();
-
-?>
