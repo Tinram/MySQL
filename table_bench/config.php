f0d6f90fe2886528f774d65c154a74fd7ec4d4cf
Tinram <Tinram@users.noreply.github.com>
2020-12-02 15:58:31 +0000

    update table_bench: config.php

--- table_bench/config.php
+++ table_bench/config.php
@@ -1,54 +1,52 @@
 <?php
 
 /**
-	* Configuration file for TableBench class.
+    * Configuration file for TableBench class.
 */
 
+declare(strict_types=1);
 
-interface Configuration {
+interface Configuration
+{
+    /* database connection credentials */
 
-	const
+    const HOST = 'localhost';
+    const DATABASE_NAME = 'table_bench';
+    const TABLE_NAME = 'bench';
 
-		/* database connection details */
+    const DATABASE_USERNAME = 'bencher';
+    const DATABASE_PASSWORD = 'P@55w0rd';
 
-		HOST = 'localhost',
-		DATABASE_NAME = 'table_bench',
-		TABLE_NAME = 'bench',
-
-		DATABASE_USERNAME = 'bencher',
-		DATABASE_PASSWORD = 'password',
-
-		DATABASE_CHARSET = 'utf-8',
-		DATABASE_ENGINE = 'mysqli', # mysqli or pdo
-
-
-		/* MySQL configuration */
-
-		# number of table rows to add
-		NUM_ROWS = 50,
-
-		# toggle disable keys before table operations
-		DISABLE_INDEXES = FALSE,
-
-		# toggle parameterized queries
-		PREPARED_STATEMENTS = FALSE,
-
-		# toggle transactions
-		TRANSACTIONS = FALSE,
+    const DATABASE_CHARSET = 'utf-8';
+    const DATABASE_ENGINE = 'mysqli'; # mysqli or pdo
 
 
-		/* mysqld parameters - only possible with DATABASE_USERNAME = root */
-		/* other parameters require my.cnf editing */
+    /* MySQL configuration */
 
-		MAX_ALLOWED_PACKET = 268435456,
+    # number of table rows to add
+    const NUM_ROWS = 50;
 
-		INNODB_FLUSH_LOG_AT_TRX_COMMIT = 2;
+    # toggle disable keys before table operations
+    const DISABLE_INDEXES = false;
+
+    # toggle parameterized queries
+    const PREPARED_STATEMENTS = false;
+
+    # toggle transactions
+    const TRANSACTIONS = false;
 
 
-		public function DBConnect();
+    /*
+        * mysqld parameters: only possible with DATABASE_USERNAME = 'root'
+        * other parameters require my.cnf editing.
+    */
+
+    const MAX_ALLOWED_PACKET = 268435456;
+
+    const INNODB_FLUSH_LOG_AT_TRX_COMMIT = 2;
 
 
-		public function getResults();
+    public function DBConnect(): void;
+
+    public function getResults(): string;
 }
-
-?>
